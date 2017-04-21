a87226d981eb8a9a139053b225155189e0ef78d3
Curtis Jenkins <curtisjenkins@Curtiss-MacBook-Pro.local>
2017-04-20 21:07:18 -0400

    switch to fontawesome

--- resources/assets/js/components/Members/MembersCustomActions.vue
+++ resources/assets/js/components/Members/MembersCustomActions.vue
@@ -1,7 +1,7 @@
   <template>
     <div class="custom-actions">
       <!-- <button class="btn btn-sm" @click="itemAction('view-item', rowData, rowIndex)"><i class="glyphicon glyphicon-zoom-in"></i></button> -->
-      <button class="btn btn-sm" @click="itemAction('edit-item', rowData, rowIndex)"><i class="glyphicon glyphicon-pencil"></i></button>
+      <a class="" @click="itemAction('edit-item', rowData, rowIndex)"><i class="fa fa-address-card-o fa-lg" aria-hidden="true"></i></a>
       <!-- <button v-if="rowData.authUserCanDelete" class="btn btn-sm" @click="itemAction('delete-item', rowData, rowIndex)"><i class="glyphicon glyphicon-trash"></i></button> -->
     </div>
   </template>
