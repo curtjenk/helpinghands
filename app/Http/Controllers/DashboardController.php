62caa3f8f51458cdcf59c3ccb5e8dc33b9ba979a
Curtis Jenkins <curtisjenkins@Curtiss-MacBook-Pro.local>
2017-07-04 21:15:32 -0400

    Query constrain to organization. Additional query

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -28,23 +28,49 @@ class DashboardController extends Controller
     public function index()
     {
         $user = Auth::user();
+        $numMembers = App\User::where('organization_id',$user->organization_id)->count();
         foreach (App\EventType::all() as $et) {
             $eventTypes[$et->name] = 0;
         }
-        $data['EventTypesOverTime'] = $this->events_over_time($eventTypes);
-        $data['TotalByType'] = $this->total_by_type($eventTypes);
-        $data['ParticpationRate'] = $this->overall_particiption();
+        $data['EventTypesOverTime'] = $this->events_over_time($eventTypes, $user->organization_id);
+        $data['TotalByType'] = $this->total_by_type($eventTypes, $user->organization_id);
+        $data['ParticpationRate'] = $this->overall_particiption($user->organization_id);
+        $data['ParticpationOverTime'] = $this->particiption_over_time($user->organization_id);
         return response()->json($data);
     }
-    private function overall_particiption()
+    /**
+     * [particiption_over_time description]
+     * @return [type] [description]
+     */
+    private function particiption_over_time($org_id)
     {
-        $user = Auth::user();
-        $numMembers = App\User::where('organization_id',$user->organization_id)->count();
         $query = App\Response::selectRaw(
-            "sum(case helping when true then 1 else 0 end)*100/count(*) as pct_yes,
-             sum(case helping when false then 1 else 0 end)*100/count(*) as pct_no,
-             sum(case when helping is null  then 1 else 0 end)*100/count(*) as pct_no_reply"
-            )->get();
+            "to_char(date_start, 'YYYY mm')  as interval,
+             sum(case helping when true then 1 else 0 end)*100/count(*) as Yes,
+             sum(case helping when false then 1 else 0 end)*100/count(*) as No,
+             sum(case when helping is null  then 1 else 0 end)*100/count(*) as NoReply"
+            )
+            ->join('events', 'events.id', '=', 'responses.event_id')
+            ->where('events.organization_id', $org_id)
+            ->groupby("interval")
+            ->get();
+
+        return $query;
+    }
+    /**
+     * [overall_particiption description]
+     * @return [type] [description]
+     */
+    private function overall_particiption($org_id)
+    {
+        $query = App\Response::selectRaw(
+            "sum(case helping when true then 1 else 0 end)*100/count(*) as yes,
+             sum(case helping when false then 1 else 0 end)*100/count(*) as no,
+             sum(case when helping is null  then 1 else 0 end)*100/count(*) as noreply"
+            )
+            ->join('events', 'events.id', '=', 'responses.event_id')
+            ->where('events.organization_id', $org_id)
+            ->get();
         $participation = [];
         foreach($query as $key=>$value)
         {
@@ -56,12 +82,13 @@ class DashboardController extends Controller
      *  Get Year-To-Date total for each event type
      * @return [type] [description]
      */
-    private function total_by_type($eventTypes)
+    private function total_by_type($eventTypes, $org_id)
     {
         $totalByType = [];
         $query = App\Event::select(DB::raw("event_types.name, count(*) as num"))
             ->join('event_types', 'event_types.id', '=', 'events.event_type_id')
             ->where('date_start', '>=', date('Y-01-01'))
+            ->where('events.organization_id', $org_id)
             ->groupby("name")
             ->get();
         foreach($eventTypes as $k=>$t) {
@@ -80,12 +107,13 @@ class DashboardController extends Controller
      * @param  [type] $eventTypes [description]
      * @return [type]             [description]
      */
-    private function events_over_time($eventTypes)
+    private function events_over_time($eventTypes, $org_id)
     {
         $eventsOverTime = [];
         $query = App\Event::select(DB::raw("to_char(date_start, 'YYYY mm')  as interval,
             event_types.name, count(*) as num"))
             ->join('event_types', 'event_types.id', '=', 'events.event_type_id')
+            ->where('events.organization_id', $org_id)
             ->groupby("interval")
             ->groupby("name")
             ->orderby("interval")
