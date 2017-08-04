e12c8471517f8233f021d34f67c2d69d7e47d562
Curtis Jenkins <curtisjenkins@Curtiss-MBP.lan>
2017-08-03 20:27:14 -0400

    fix event show header

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -4,7 +4,7 @@
 <main>
     <section class="page-header">
         <div class="container">
-            <div class="pull-left header">Service / Fellowship Event</div>
+            <div class="pull-left header"> Event</div>
             <div class="pull-right">
                 <a class="btn btn-default" href="{{ url('/event') }}"><i class="fa fa-list"></i> Events</a>
             @if($event->statusOpen())
