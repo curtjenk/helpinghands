bc72c3a2bf6414a63a636eb1f9dc45d822c01d20
Curtis Jenkins <>
2018-02-11 20:28:07 -0500

    blah

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,69 +22,15 @@
 </head>
 <body>
     <div id="app">
-<<<<<<< HEAD
-        <nav class="navbar navbar-expand-lg  bg-dark fixed-top">
-            <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggler collapsed float-left" data-toggle="collapse" data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="navbar-toggler-icon"></span>
-            </button>
-=======
         <nav-top
             :user="{{ Auth::check() ? Auth::user() : 'null' }}"
             :roles="{{ Auth::check() ? Auth::user()->roles()->get()->pluck('name') : '[]' }}"
             :permissions="{{ Auth::check() ? Auth::user()->permissions()->get()->pluck('name') : '[]' }}"
         >
         </nav-top>
->>>>>>> 9579919d70163306d01961c9a1ad686fc484def3
 
 
-<<<<<<< HEAD
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                        {{-- <li><a href="{{ route('register') }}">Register</a></li> --}}
-                    @else
-                        <li class="nav-item dropdown">
-                            <a  href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li class="dropdown-item">
-                                    <a href="{{ url('/member/'.Auth::user()->id.'/edit') }}"><i class="fa fa-cog fa-fw"></i> Profile</a></li>
-                                @can ('administer')
-                                <li class="dropdown-item">
-                                    <a href="{{ url('/administrator') }}"><i class="fa fa-th-large fa-fw"></i> Administrator</a></li>
-                                @endcan
-                                <li class="dropdown-item">
-                                    <a href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-
-        </nav>
-        <div class="mt-5">
-=======
         <div class="">
->>>>>>> 9579919d70163306d01961c9a1ad686fc484def3
             @yield('content')
         </div>
 
