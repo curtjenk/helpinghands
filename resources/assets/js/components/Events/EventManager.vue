52a2833fae5947391c27e64b529bc002dfe9a482
Curtis Jenkins <>
2018-04-24 20:24:07 -0400

    quill styles

--- resources/assets/js/components/Events/EventManager.vue
+++ resources/assets/js/components/Events/EventManager.vue
@@ -307,6 +307,11 @@ import VueTimepicker from "vue2-timepicker";
 
 //Quill.js based component
 import { VueEditor } from 'vue2-editor'
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 
 export default {
   mixins: [commonMixins],
